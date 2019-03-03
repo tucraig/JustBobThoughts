b40040426020819942bb644b1d9b5c9bb1bd2692
Tucker <35579682+tucraig@users.noreply.github.com>
2019-03-03 17:09:28 -0500

    Update download_clips.py

--- download_clips.py
+++ download_clips.py
@@ -62,7 +62,6 @@ def download_clips(selected_season = None):
                                 time_stamp=time.strftime("%M_%S", time.gmtime(float(start)))
                                 clip.subclip(float(start),float(start) + float(dur)).write_videofile("clips/S{}E{}-{}-{}-{}.mp4".format(season+1,episode+1,time_stamp,text,clip_titles.count(text)),fps=30,codec='libx264')
                                 # clip.subclip(float(start),float(start) + float(dur)).write_videofile("clips/S1E{}-{}-{}.gif".format(episode+1,text,clip_titles.count(text)),fps=30,codec='gif')
-                                print(time_stamp)
                                 clip_titles.append(text)
                                 clip.reader.close()
                                 clip.audio.reader.close_proc()
